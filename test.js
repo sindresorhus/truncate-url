0f221d5dda8148928115316e9b727dce0f970c9d
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:08:55 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import fn from './';
 
-test('should truncate url', function (t) {
+test('should truncate url', t => {
 	t.is(fn('http://sindresorhus.com/foo/bar/baz/faz', 30), 'http://sindresorhus.com/…/faz');
 	t.is(fn('http://example.com/a/cool/page/that-is-really-deeply/nested/', 40), 'http://example.com/…/nested/');
 	t.is(fn('http://example.com/a/b/c/d', 24).length, 24);
 	t.is(fn('http://example.com/a/b/c', 24), 'http://example.com/a/b/c');
 	t.is(fn('http://example.com/a/b/cd', 24), 'http://example.com/…/cd');
-	t.end();
 });
 
-test('should pass corner case', function (t) {
-	var result = fn('http://example.com/a/b/cd', 24);
+test('should pass corner case', t => {
+	const result = fn('http://example.com/a/b/cd', 24);
 	t.true(result.length <= 24, result + ' is > 24');
-	t.end();
 });
