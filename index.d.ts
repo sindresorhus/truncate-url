76e69dd4c25f41ae5a3c49f81bd127fe0c8e2036
Cerberooo <Cerberooo@users.noreply.github.com>
2021-04-22 09:21:12 +0200

    adds type test & refactors to requirements

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,18 @@
-declare module 'truncate-url' {
-    function truncateUrl(url: string, maxLength: number): string;
+/**
+Truncate a URL to a specific length.
 
-    export = truncateUrl;
-}
+@param url - The URL to be shortened.
+@param maxLength - The maximum length of the shortened URL.
+@returns The shortened URL.
+
+@example
+```
+import truncateUrl from 'truncate-url';
+
+truncateUrl('https://sindresorhus.com/foo/bar/baz/faz', 30);
+//=> 'https://sindresorhus.com/…/faz'
+```
+	*/
+declare function truncateUrl(url: string, maxLength: number): string;
+
+export default truncateUrl;
